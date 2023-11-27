c62753ba4239ea8370376692f33903fa05e2420d
Kangtae.Kim <tt997768@gmail.com>
2023-11-27 23:19:51 +0900

    bug fix

---
+++ bin/dart_exam.dart
@@ -0,0 +1,5 @@
+import 'package:dart_exam/dart_exam.dart' as dart_exam;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_exam.calculate()}!');
+}
---
+++ test/dart_exam_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_exam/dart_exam.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
