30530caa8943d96ede5478d8ce1dfa85ec993106
kimkangtae <tt997768@gmail.com>
2023-11-29 22:34:17 +0900

    codewar practice

--- lib/11_29/codewar.dart
+++ lib/11_29/codewar.dart
@@ -18,17 +18,11 @@ List<List<int>> pyramid(int n) {
 
   List<List<int>> a = [[1]];
 
-  for(int i = 2; i < n ;i ++){
-    List<int> newRow = [];
-    List<int> prevRow = a[i-2];
-    newRow.add(1);
-    for(int j = 1; j< i; j++){
-      newRow.add(prevRow[j-1] + prevRow[j]);
-    }
-    newRow.add(1);
-    a.add(newRow);
-
-
+  for(int i = 1; i < n ;i ++){
+    List<int> last = List.from(a[i-1]);
+    print('$last + i $i');
+    last.add(1);
+    a.add(last);
   }
 
   return a;
