2194cc7d6839c63d8558b64ac12f8e611aadc648
kimkangtae <tt997768@gmail.com>
2023-11-30 17:27:12 +0900

    연습문제 1-4

--- lib/11_30/01_practice_cleric.dart
+++ lib/11_30/01_practice_cleric.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 class Cleric{
   final String name;
   int HP = 50;
@@ -13,4 +15,29 @@ class Cleric{
     }
   }
 
+  int pray(int second){
+    int plusMP = 0;
+    int tmpMP = 0;
+    plusMP = second + Random().nextInt(3);
+    MP += plusMP;
+    tmpMP = MP;
+
+    if(MP > MaxMP){
+      MP = MaxMP;
+      return (plusMP -(tmpMP - MaxMP));
+    }else{
+      return plusMP;
+    }
+  }
 }
+
+// void main(){
+//   final Cleric cleric = Cleric("karl");
+//   cleric.MP = 2;
+//   cleric.HP = 10;
+//   int a = cleric.pray(5);
+//   cleric.selfAid();
+//   print(a);
+//   print(cleric.MP);
+//   print(cleric.HP);
+// }
