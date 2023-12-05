d41a9b86e567b78d6123841d4c211064d816dc58
kimkangtae <tt997768@gmail.com>
2023-12-05 14:07:03 +0900

    연습문제 11-1

---
+++ lib/23_12_05/01_homework_11.dart
@@ -0,0 +1,9 @@
+// 연습문제 11-1 다음 중에서 “잘못 된 상속" 인 것을 모두 구하시오
+//
+// 슈퍼클래스  서브클래스
+// Person   Student => true
+// Car      Engine => false
+// Father   Child => false
+// Food     Susi => true
+// SuperMan Man => false
+
