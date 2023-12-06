15bf1aa7f1cc8b0ccf90fdb65b3ad3842f187de9
kimkangtae <tt997768@gmail.com>
2023-12-06 20:23:54 +0900

    starcraft 에러 수정

--- lib/23_12_06/starcraft/main.dart
+++ lib/23_12_06/starcraft/main.dart
@@ -27,34 +27,34 @@ void main(List<String> args) {
   Mutalisk mutalisk7 = Mutalisk(hp: 100);
 
   //테란 테스트
-  scv.earn();
-  print('현재 탱크의 HP: ${tank.hp}');
-  scv.repair(tank);
+  // scv.earn();
+  // print('현재 탱크의 HP: ${tank.hp}');
+  // scv.repair(tank);
   //scv는 생물유닛을 수리 못한다
   // scv.repair(marine);
 
-  print('현재 마린의 HP: ${marine.hp}');
-  medic.heal(marine);
-  print('현재 SCV의 HP: ${marine.hp}');
+  // print('현재 마린의 HP: ${marine.hp}');
+  // medic.heal(marine);
+  // print('현재 SCV의 HP: ${marine.hp}');
   // SCV는 medic의 힐을 받을 수 있다.
-  medic.heal(scv);
+  // medic.heal(scv);
   // medic은 SCV 제외 기계유닛을 힐을 못한다.
   // medic.heal(dragun);
-
-  // 저그 테스트
-  print('현재 뮤탈리스크의 HP: ${mutalisk.hp}');
-  //뮤탈리스크는 생물유닛이라 메딕의 힐을 받을 수 있음.
-  medic.heal(mutalisk);
-  //저그 종족은 autoHeal 기능이 있음.
-  mutalisk.autoHeal();
-
-  // 프로토스 테스트
-  print('현재 질럿의 HP: ${zealot.hp}');
-  medic.heal(zealot);
-  print('현재 질럿의 SHIELD: ${zealot.shield}');
-  // 프로토스 는 방어막을 가지며 서서히 회복한다.
-  zealot.recoveryShield();
-
+  //
+  // // 저그 테스트
+  // print('현재 뮤탈리스크의 HP: ${mutalisk.hp}');
+  // //뮤탈리스크는 생물유닛이라 메딕의 힐을 받을 수 있음.
+  // medic.heal(mutalisk);
+  // //저그 종족은 autoHeal 기능이 있음.
+  // mutalisk.autoHeal();
+  //
+  // // 프로토스 테스트
+  // print('현재 질럿의 HP: ${zealot.hp}');
+  // medic.heal(zealot);
+  // print('현재 질럿의 SHIELD: ${zealot.shield}');
+  // // 프로토스 는 방어막을 가지며 서서히 회복한다.
+  // zealot.recoveryShield();
+  //
   army = addArmy(army, medic);
   army = addArmy(army, marine);
   army = addArmy(army, zealot);
@@ -69,12 +69,12 @@ void main(List<String> args) {
   army = addArmy(army, mutalisk5); // 여기까지 12
   army = addArmy(army, mutalisk6);
   army = addArmy(army, mutalisk7);
-
+  //
   print(army.length);
   print(army);
 }
 
-addArmy(List<ArmyUnionable> army, ArmyUnionable unit) {
+List<ArmyUnionable> addArmy(List<ArmyUnionable> army, ArmyUnionable unit) {
   if (army.length < 12) {
     army.add(unit);
     return army;
--- lib/23_12_06/starcraft/terran/bionic/interface/bionic.dart
+++ lib/23_12_06/starcraft/terran/bionic/interface/bionic.dart
@@ -1,4 +1,5 @@
+import '../../../interface/army_unionable.dart';
 import '../../../interface/medic_curable.dart';
 import '../../interface/terran.dart';
 
-abstract class Bionic extends Terran implements MedicCurable {}
+abstract class Bionic extends Terran implements MedicCurable, ArmyUnionable {}
