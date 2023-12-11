c77b752107f764ec8d819794fa5893ff4a668e34
kimkangtae <tt997768@gmail.com>
2023-12-11 14:50:42 +0900

    예외 연습문제 2

--- lib/23_12_11/00_homework_exception.dart
+++ lib/23_12_11/00_homework_exception.dart
@@ -1,11 +1,21 @@
 // 연습문제 1
 // 다음과 같은 프로그램을 작성, 실행 후 runtime error 를 발생하시오
 
+// 연습문제 2
+// 연습 1 에서 작성한 코드를 수정하여, try-catch() 문을 사용하여 예외처리를 하시오.
+// 예외가 발생하면 0으로 처리
 
 void main() {
   final numString = '10.5';
 
-  int num = int.parse(numString);
+  int num;
+
+  try {
+    num = int.parse(numString);
+  } catch (e) {
+    print('Exception 발생!! num => 0 \n$e');
+    num = 0;
+  }
 
   print(num);
 }
