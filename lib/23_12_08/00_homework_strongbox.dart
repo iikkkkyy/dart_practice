850e679392b978508ebf7bccca797fd5eb4905f5
kimkangtae <tt997768@gmail.com>
2023-12-11 09:57:46 +0900

    Strong box 수정 (E를 활용을 안함)

--- lib/23_12_08/00_homework_strongbox.dart
+++ lib/23_12_08/00_homework_strongbox.dart
@@ -6,35 +6,30 @@ class StrongBox<E> {
   // 금고에는 1개의 인스턴스를 담을 수 있음
   // 열쇠의 종류를 나타내는 필드 변수
   KeyType _keyType;
+  E? _someThing;
 
   // 최초 카운트 선언
-  int padlockKeyCount = 0;
-  int buttonKeyCount = 0;
-  int dialKeyCount = 0;
-  int fingerKeyCount = 0;
+  int _count = 0;
 
   //put() 메서드로 인스턴스를 저장하고
   //열쇠의 종류를 받는 생성자
-  void put(KeyType keyType) {
-    _keyType = keyType;
+  void put(E someThing) {
+    _someThing = someThing;
   }
 
   //get() 메서드로 인스턴스를 얻을 있음
   //get() 으로 얻을 때는 별도의 타입 캐스팅을 사용하지 않아도 됨 .?.?
-  String? get() {
+  E? get() {
+    _count++;
     switch (_keyType) {
       case KeyType.padlock:
-        padlockKeyCount++;
-        return padlockKeyCount > 1024 ? "사용 횟수 초과" : null;
+        return _count > 1024 ? _someThing : null;
       case KeyType.button:
-        buttonKeyCount++;
-        return buttonKeyCount > 10000 ? "사용 횟수 초과" : null;
+        return _count > 10000 ? _someThing : null;
       case KeyType.dial:
-        dialKeyCount++;
-        return dialKeyCount > 30000 ? "사용 횟수 초과" : null;
+        return _count > 30000 ? _someThing : null;
       case KeyType.finger:
-        fingerKeyCount++;
-        return fingerKeyCount > 1000000 ? "사용 횟수 초과" : null;
+        return _count > 1000000 ? _someThing : null;
     }
   }
 
@@ -56,25 +51,22 @@ void main() {
 
   // Test
   // button 타입의 상자 생성
-  StrongBox box = StrongBox(keyType: KeyType.button);
+  StrongBox<String> box = StrongBox(keyType: KeyType.button);
 
   // get으로 호출 ( 카운트 1 증가 null값 리턴받을 것이다)
   print('get 호출!! : ${box.get()}');
 
+  box.put('안녕하세요?');
   // button 타입을 10000번 채우게 추가호출 후 테스트
   for (int i = 0; i < 10000; i++) {
     box.get();
   }
   print('get 호출!! : ${box.get()}');
 
-  // put으로 keytype 변경 및 테스트
-  box.put(KeyType.padlock);
-  print('get 호출!! : ${box.get()}');
-  box.put(KeyType.button);
-  print('get 호출!! : ${box.get()}');
-  box.put(KeyType.padlock);
   for (int i = 0; i < 1024; i++) {
     box.get();
   }
   print('get 호출!! : ${box.get()}');
 }
+
+// E로 put 받은거를 활용.?
