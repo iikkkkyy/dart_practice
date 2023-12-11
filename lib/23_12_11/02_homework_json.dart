2794f7b7021ca2e2e7d6a64109d757bc9e34be7e
kimkangtae <tt997768@gmail.com>
2023-12-11 15:47:32 +0900

    json 연습문제

--- lib/23_12_11/02_homework_json.dart
+++ lib/23_12_11/02_homework_json.dart
@@ -2,7 +2,6 @@
 // 총무부 리더 ‘홍길동(41세)’의 인스턴스를 생성하고 직렬화하여 company.txt 파일에 쓰는 프로그램을 작성하시오.
 // 직렬화를 위해 위의 2개 클래스를 일부 수정해도 됩니다.
 
-
 import 'dart:io';
 
 class Employee {
@@ -19,9 +18,8 @@ class Employee {
   }
 
   Employee.fromJson(Map<String, dynamic> json)
-    : name = json['name'],
-      age = json['age'];
-
+      : name = json['name'],
+        age = json['age'];
 }
 
 class Department {
@@ -33,7 +31,7 @@ class Department {
   Map<String, dynamic> toJson() {
     return {
       'name': name,
-      'leader' : leader.toJson(),
+      'leader': leader.toJson(),
     };
   }
 
@@ -55,5 +53,4 @@ void main() {
   // 직렬화된 결과 company.txt 에 작성
   final myFile = File('company.txt');
   myFile.writeAsStringSync(department.toJson().toString());
-
 }
