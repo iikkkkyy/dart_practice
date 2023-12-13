6f1a2571887ba3d2e978290c4c0e102073578112
kimkangtae <tt997768@gmail.com>
2023-12-13 14:56:07 +0900

    연습문제 1 파일 처리

---
+++ lib/23_12_13/00_homework_async.dart
@@ -0,0 +1,28 @@
+// 연습문제 1. 파일처리
+// sample.csv 파일을 읽어와서 “한석봉" 이라는 문자열이 있는지 찾고,
+// 있다면 sample_copy.csv 파일에 “김석봉"으로 수정하는 함수를 작성하시오.
+// (async - await 사용할 것)
+
+import 'dart:io';
+
+void main() {
+  fileEdit('sample.csv', 'sample_copy.csv');
+}
+
+Future<void> fileEdit(String src, String dst) async {
+  final word = await readFile(src);
+  if (word.contains('한석봉')) {
+    await makeFile(dst, word.replaceAll('한석봉', '김석봉'));
+  }
+}
+
+Future<String> readFile(String src) async {
+  final originalFile = File(src);
+  final word = await originalFile.readAsString();
+  return word;
+}
+
+Future<void> makeFile(String dst, String word) async {
+  final copyFile = File(dst);
+  await copyFile.writeAsString(word);
+}
