4059b7b6d5ca1be5b4098e873ca38e1c22335e8e
kimkangtae <tt997768@gmail.com>
2023-12-14 15:57:08 +0900

    연습문제 4. 다운로드 시간,용량 표시

--- lib/23_12_14/02_http_file_download.dart
+++ lib/23_12_14/02_http_file_download.dart
@@ -1,12 +1,18 @@
 // 연습문제 3. 사진 다운로드
+// 연습문제 4. 다운로드 시간, 용량 표시
 import 'dart:convert';
 import 'dart:io';
 import 'dart:typed_data';
 import 'package:http/http.dart' as http;
 
 void main() async {
+  final stopWatch = Stopwatch()..start();
+  print('다운로드 시작');
   final imageBytes = await downloadImage('https://alimipro.com/favicon.ico');
+  print('다운로드 끝');
   final imageFile = await saveFile(imageBytes, 'favicon.ico');
+  print('소요시간 : ${(stopWatch.elapsed.toString())}');
+  print('용량 : ${imageBytes.lengthInBytes}bytes');
 }
 
 // 네트워크 상의 사진 파일을 다운로드 받는 함수를 작성한다.
