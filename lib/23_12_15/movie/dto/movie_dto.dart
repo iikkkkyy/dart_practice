32b9a1e067a5a69f3158020e8075ed458a409ed1
kimkangtae <tt997768@gmail.com>
2023-12-15 16:48:32 +0900

    연습문제 movie dto

---
+++ lib/23_12_15/movie/dto/movie_dto.dart
@@ -0,0 +1,275 @@
+/// dates : {"maximum":"2024-01-10","minimum":"2023-12-20"}
+/// page : 1
+/// results : [{"adult":false,"backdrop_path":"/k1KrbaCMACQiq7EA0Yhw3bdzMv7.jpg","genre_ids":[16,10751,10402,14,35],"id":901362,"original_language":"en","original_title":"Trolls Band Together","overview":"전 세계 모든 트롤을 열광케 했던 최고의 아이돌 그룹 ‘브로존’. 역대급 무대 실수와 형제 간의 불화로 결국 해체한 뒤, 모두에게 잊혀 간다. 그러던 어느 날, ‘브로존’의 황금막내 ‘브랜치’는 메인보컬 ‘플로이드’가 슈퍼스타 ‘벨벳’과 ‘비니어’에게 잡혀 재능을 빼앗기고 있다는 소식을 듣는다. 그를 구하기 위해서는 흩어져 있는 ‘브로존’을 재결합하고 완벽한 화음을 되찾아야 하는데… 12월, 가장 짜릿한 컴백 무대의 시작!","popularity":968.582,"poster_path":"/7M2pc9OboapgtoBbkU49Aim7O5B.jpg","release_date":"2023-10-12","title":"트롤 밴드 투게더","video":false,"vote_average":7.169,"vote_count":355},{"adult":false,"backdrop_path":"/AucuqDaUW4z1oT5XyL71qwRY2F6.jpg","genre_ids":[27,53],"id":983507,"original_language":"no","original_title":"Meg, deg & Frank","overview":"","popularity":722.698,"poster_path":"/9DPG1gxLwV2oyFdHq3SnISsWbse.jpg","release_date":"2022-06-03","title":"Meg, deg & Frank","video":false,"vote_average":6.8,"vote_count":99},{"adult":false,"backdrop_path":"/bWIIWhnaoWx3FTVXv6GkYDv3djL.jpg","genre_ids":[878,27,28],"id":940721,"original_language":"ja","original_title":"ゴジラ-1.0","overview":"","popularity":359.215,"poster_path":"/buvBq2zLP7CcJth8tjrI4znvfEO.jpg","release_date":"2023-11-03","title":"고지라-1.0","video":false,"vote_average":8.198,"vote_count":121},{"adult":false,"backdrop_path":"/jXJxMcVoEuXzym3vFnjqDW4ifo6.jpg","genre_ids":[28,12,14],"id":572802,"original_language":"en","original_title":"Aquaman and the Lost Kingdom","overview":"아틀란티스 왕국을 이끌 왕의 자리에 오른 아쿠아맨. 그 앞에 블랙 만타가 세상을 뒤흔들 강력한 지배 아이템 블랙 트라이던트를 손에 넣게 된다. 그동안 겪지 못 했던 최악의 위협 속 아쿠아맨은 블랙 만타와 손을 잡았던 이복 동생 옴 없이는 절대적 힘이 부족한 상황. 바다를 지배할 슈퍼 히어로가 세상의 판도를 바꾼다!","popularity":382.701,"poster_path":"/eDps1ZhI8IOlbEC7nFg6eTk4jnb.jpg","release_date":"2023-12-20","title":"아쿠아맨과 로스트 킹덤","video":false,"vote_average":0.0,"vote_count":0},{"adult":false,"backdrop_path":"/8RBwd37p2p48lUmZuWBLSkFLprd.jpg","genre_ids":[27,53,35],"id":1113278,"original_language":"en","original_title":"It's a Wonderful Knife","overview":"","popularity":294.112,"poster_path":"/bnm5NntrQBcG3P7DbO00v2FmXm8.jpg","release_date":"2023-11-10","title":"It's a Wonderful Knife","video":false,"vote_average":5.241,"vote_count":29},{"adult":false,"backdrop_path":"/ehumsuIBbgAe1hg343oszCLrAfI.jpg","genre_ids":[16,10751,14,12],"id":1022796,"original_language":"en","original_title":"Wish","overview":"마음 속 깊이 ‘소원’을 품고 있는 모두를 위한 디즈니의 마법! 용감한 ‘아샤’와 신비로운 ‘별’의 특별한 여정 & 소원과 얽힌 비밀스러운 힘을 지닌 ‘매그니피코 왕’의 숨겨진 스토리까지! 2024년 1월, 믿고 듣는 디즈니표 명품 OST와 함께 아름다운 로사스 왕국에서 펼쳐지는 가슴 벅찬 이야기!","popularity":261.932,"poster_path":"/lnOgP8Fl2dQ73d4Qu24O2zcKFFS.jpg","release_date":"2023-11-13","title":"위시","video":false,"vote_average":6.649,"vote_count":111},{"adult":false,"backdrop_path":null,"genre_ids":[99],"id":1192578,"original_language":"es","original_title":"Elijo creer","overview":"","popularity":185.387,"poster_path":"/tAX05FIcjnllogMa2r0FmamNhpU.jpg","release_date":"2023-12-07","title":"Elijo creer","video":false,"vote_average":8.0,"vote_count":1},{"adult":false,"backdrop_path":"/pA3vdhadJPxF5GA1uo8OPTiNQDT.jpg","genre_ids":[28,18],"id":678512,"original_language":"en","original_title":"Sound of Freedom","overview":"로시오와 미겔 오누이를 비롯한 온두라스 아이들이 지젤의 아동 인신매매 조직에 납치된다. 한편 미 국토안보부 아동대상 인터넷범죄 전담팀(HSI) 특수요원 팀 발라드는 아동 포르노를 배포하던 성범죄자 오신스키를 검거한다. 팀은 오신스키를 이용해 아동 성착취범 얼을 체포하고 미겔을 구출한다. 미겔은 팀에게 로시오를 구해 달라고 부탁하며 누나의 목걸이를 건넨다. 미겔이 아버지 품으로 돌아간 후, 팀은 로시오를 찾기 위해 콜롬비아 카르타헤나로 향한다. 팀은 현지 경찰 호르헤를 통해 카르텔 회계사 출신의 뱀피로를 소개받는다. 팀은 이 일을 하는 이유가 \"하나님의 아이들이 매매의 대상이 되어서는 안 되기 때문\"이라고 말한다. 팀과 뱀피로는 아동 섹스 클럽을 미끼로 인신매매범들을 유인하기로 계획한다. HSI의 프로스트가 팀에게 작전을 중지하고 미국으로 돌아오라고 지시한다. 팀은 아이들을 구하기 위해 사직서를 제출하고, 부동산 개발회사 CEO 파블로를 작전에 합류시키는데...","popularity":207.612,"poster_path":"/qA5kPYZA7FkVvqcEfJRoOy4kpHg.jpg","release_date":"2023-07-03","title":"사운드 오브 프리덤","video":false,"vote_average":8.085,"vote_count":1594},{"adult":false,"backdrop_path":"/ptz5ETMxDoRRiE69BVuIxJzyTEO.jpg","genre_ids":[16,35,12,10751],"id":940551,"original_language":"en","original_title":"Migration","overview":"가족을 과잉보호하는 아빠 ‘맥’ 때문에 평생을 작은 연못에서 안전하게 살아온 말러드 가족. 하지만 호기심 가득한 남매 ‘댁스’와 ‘그웬’을 위해 새로운 세상을 모험하고 싶은 엄마 ‘팸’의 설득으로 가족들은 항상 꿈꿔온 자메이카로 생애 첫 가족 모험을 떠나기로 한다! 설렘 넘치는 시작과 달리 태풍을 만나 길을 잃고, 낯선 친구들을 만나고, 위험 가득한 뉴욕에 불시착하게 된다. 인생 처음으로 모든 계획이 틀어지고 위기에 빠진 말러드 가족은 서툴지만 서로를 의지하며 모험을 계속한다. 새로운 세상, 함께라면 두려울 것 없어!","popularity":200.392,"poster_path":"/2xE3NI6zElWhwN9WJ92fqbZKmzZ.jpg","release_date":"2023-12-06","title":"인투 더 월드","video":false,"vote_average":7.659,"vote_count":22},{"adult":false,"backdrop_path":"/ktHEdqmMWC1wdfPRMRCTZe2OISL.jpg","genre_ids":[27,53,9648],"id":1071215,"original_language":"en","original_title":"Thanksgiving","overview":"","popularity":153.124,"poster_path":"/f5f3TEVst1nHHyqgn7Z3tlwnBIH.jpg","release_date":"2023-11-16","title":"땡스기빙","video":false,"vote_average":6.349,"vote_count":106},{"adult":false,"backdrop_path":"/qElNES0sHVQcbzvGrTx7ccpGzij.jpg","genre_ids":[878,28,18],"id":842675,"original_language":"zh","original_title":"流浪地球2","overview":"태양계 소멸의 위기를 맞은 인류는 지구 표면에 거대한 엔진을 달아 궤도를 옮기는 ‘유랑지구 프로젝트’에 돌입한다. 하지만, 데이터베이스로 영생을 가지려는 ‘디지털 라이프’와 대립하게 되며 프로젝트는 난관에 부딪히게 되고, 그 과정 속 달과의 충돌이라는 예상치 못한 대재앙에 놓이게 되는데… 살아남기 위한 선택은 단 하나, 달을 파괴하라!","popularity":164.159,"poster_path":"/pajq0n6FZivirCg1ifpe4zYKvsA.jpg","release_date":"2023-01-22","title":"유랑지구 2","video":false,"vote_average":7.241,"vote_count":408},{"adult":false,"backdrop_path":"/jBOyi3ibJbVa1imTCcddPUxTVAV.jpg","genre_ids":[35,27,14],"id":956920,"original_language":"no","original_title":"There's Something in the Barn","overview":"","popularity":127.064,"poster_path":"/gncQ1homJA8exLRwELjR9vSYWC7.jpg","release_date":"2023-11-02","title":"There's Something in the Barn","video":false,"vote_average":7.291,"vote_count":44},{"adult":false,"backdrop_path":"/97MOhHIgU6ZdLcB9DrAhx3WAqrU.jpg","genre_ids":[18,35],"id":839369,"original_language":"en","original_title":"May December","overview":"신문 1면을 장식한 스캔들의 주인공 그레이시(줄리안 무어), 그리고 20년 후 그녀를 연기하게 된 배우 엘리자베스(나탈리 포트만)의 묘한 만남을 담은 영화","popularity":141.274,"poster_path":"/yibtHDMO70RueiEmtrcJeTiiHFo.jpg","release_date":"2023-11-16","title":"메이 디셈버","video":false,"vote_average":6.778,"vote_count":97},{"adult":false,"backdrop_path":"/a0GM57AnJtNi7lMOCamniiyV10W.jpg","genre_ids":[16,12,14,18],"id":508883,"original_language":"ja","original_title":"君たちはどう生きるか","overview":"화재로 어머니를 잃은 11살 소년 ‘마히토’는 아버지와 함께 어머니의 고향으로 간다. 어머니에 대한 그리움과 새로운 보금자리에 적응하느라 힘들어하던 ‘마히토’ 앞에 정체를 알 수 없는 왜가리 한 마리가 나타나고, 저택에서 일하는 일곱 할멈으로부터 왜가리가 살고 있는 탑에 대한 신비로운 이야기를 듣게 된다. 그러던 어느 날, ‘마히토’는 사라져버린 새엄마 ‘나츠코’를 찾기 위해 탑으로 들어가고, 왜가리가 안내하는 대로 이세계(異世界)의 문을 통과하는데…!","popularity":127.235,"poster_path":"/kmoScy628A6JWv8mmd2ofrYv16T.jpg","release_date":"2023-07-14","title":"그대들은 어떻게 살 것인가","video":false,"vote_average":7.25,"vote_count":262},{"adult":false,"backdrop_path":"/iEFuHjqrE059SmflBva1JzDJutE.jpg","genre_ids":[16,10751,14,28,10749,10402],"id":496450,"original_language":"fr","original_title":"Miraculous - le film","overview":"마법 보석의 수호자로 인해 슈퍼히어로가 된 수줍음 많은 소녀와 인기 많은 소년. 정반대에 끌리는 두 사람은 서로에게조차 정체를 밝힐 수 없는데.","popularity":136.283,"poster_path":"/iu4qpzsyBcnWhiwh1BxTAJefTmS.jpg","release_date":"2023-07-05","title":"미라큘러스: 레이디버그와 블랙캣, 더 무비","video":false,"vote_average":7.793,"vote_count":683},{"adult":false,"backdrop_path":"/3YPP5Buu2lWyRU2YTYRtSqJikWc.jpg","genre_ids":[18,10751,14],"id":1092329,"original_language":"en","original_title":"Journey to Bethlehem","overview":"","popularity":137.695,"poster_path":"/cJgF1vjiNoRAh5UePmiPfjrw6Iv.jpg","release_date":"2023-11-09","title":"Journey to Bethlehem","video":false,"vote_average":6.833,"vote_count":6},{"adult":false,"backdrop_path":"/rron9HAuS9s7zBF8iCX1tsafxUo.jpg","genre_ids":[18,10749],"id":666277,"original_language":"en","original_title":"Past Lives","overview":"나영과 해성은 어릴 적 서로 좋아하는 감정을 가지고 있던 친구 사이다. 어느 날 나영은 캐나다로 이민을 가게 된다. 그로부터 12년 후, 노라라는 새로운 영문 이름으로 뉴욕에 거주하고 있던 나영은 페이스북을 통해 해성이 자기를 찾는다는 것을 알게 된다. 비록 온라인이지만 서로 만나게 된 해성과 나영은 반가운 마음에 하루가 멀다하고 지속적인 대화를 이어간다. 서로 직접 만나고 싶지만, 작가를 꿈꾸던 노라와 중국으로 어학연수를 가야하는 해성은 온라인 만남마저도 중단하게 된다. 그리고 다시 12년 후, 이미 다른 남자와 결혼한 나영에게 해성이 뉴욕으로 찾아오는데...","popularity":80.365,"poster_path":"/k3waqVXSnvCZWfJYNtdamTgTtTA.jpg","release_date":"2023-06-02","title":"패스트 라이브즈","video":false,"vote_average":7.919,"vote_count":353},{"adult":false,"backdrop_path":"/4kmNLwmVeT3xMciSRefE6SCi2hX.jpg","genre_ids":[80,18,53],"id":1027073,"original_language":"en","original_title":"In the Land of Saints and Sinners","overview":"","popularity":73.0,"poster_path":"/jN8JJaVpADPEU6SZX8QBCZHAFrU.jpg","release_date":"2023-10-06","title":"In the Land of Saints and Sinners","video":false,"vote_average":6.6,"vote_count":46},{"adult":false,"backdrop_path":"/gsVC7HMf4VR2XFOyqjTSklY2Tms.jpg","genre_ids":[878,35,14],"id":792307,"original_language":"en","original_title":"Poor Things","overview":"대학에서 해부학을 가르치고 있는 교수인 고드윈 벡스터는 젊고 아름다운 여성 벨라와 함께 살고 있다. 어린 아기의 지능을 가지고 있는 벨라를 고드윈은 마치 자신의 딸처럼 아끼며 그녀에게 말과 행동을 가르친다. 한편 고드윈의 제자이자 평소 그를 존경해 왔던 맥스는 고드윈 교수와 가까이 지내게 되면서 벨라를 만나게 된다. 벨라의 이상 행동에 관심을 갖기 시작하는 맥스, 고드윈 박사로부터 벨라는 얼마 전에 자살한 여자를 자신이 의학적으로 되살린 것이라는 충격적인 말을 듣게 된다. 하지만 이미 벨라에게 마음을 뺏긴 맥스는 고드윈 박사의 제안에 따라 그녀와 약혼하기로 결심하는데...","popularity":101.016,"poster_path":"/hplhTGTmKoz85Z9UBZ9kYOxtzm0.jpg","release_date":"2023-12-07","title":"가여운 것들","video":false,"vote_average":9.1,"vote_count":5},{"adult":false,"backdrop_path":"/A1g8AvP3KpDyu0iyjS6roRxK77v.jpg","genre_ids":[53,80],"id":978870,"original_language":"it","original_title":"L'ultima notte di Amore","overview":"","popularity":99.931,"poster_path":"/5FDikDWBEagwc8qGli6KVDkEK55.jpg","release_date":"2023-03-09","title":"아모레의 마지막 밤","video":false,"vote_average":7.244,"vote_count":312}]
+/// total_pages : 30
+/// total_results : 594
+
+class MovieDto {
+  MovieDto({
+    Dates? dates,
+    num? page,
+    List<Results>? results,
+    num? totalPages,
+    num? totalResults,}) {
+    _dates = dates;
+    _page = page;
+    _results = results;
+    _totalPages = totalPages;
+    _totalResults = totalResults;
+  }
+
+  MovieDto.fromJson(dynamic json) {
+    _dates = json['dates'] != null ? Dates.fromJson(json['dates']) : null;
+    _page = json['page'];
+    if (json['results'] != null) {
+      _results = [];
+      json['results'].forEach((v) {
+        _results?.add(Results.fromJson(v));
+      });
+    }
+    _totalPages = json['total_pages'];
+    _totalResults = json['total_results'];
+  }
+
+  Dates? _dates;
+  num? _page;
+  List<Results>? _results;
+  num? _totalPages;
+  num? _totalResults;
+
+  MovieDto copyWith({ Dates? dates,
+    num? page,
+    List<Results>? results,
+    num? totalPages,
+    num? totalResults,
+  }) =>
+      MovieDto(dates: dates ?? _dates,
+        page: page ?? _page,
+        results: results ?? _results,
+        totalPages: totalPages ?? _totalPages,
+        totalResults: totalResults ?? _totalResults,
+      );
+
+  Dates? get dates => _dates;
+
+  num? get page => _page;
+
+  List<Results>? get results => _results;
+
+  num? get totalPages => _totalPages;
+
+  num? get totalResults => _totalResults;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    if (_dates != null) {
+      map['dates'] = _dates?.toJson();
+    }
+    map['page'] = _page;
+    if (_results != null) {
+      map['results'] = _results?.map((v) => v.toJson()).toList();
+    }
+    map['total_pages'] = _totalPages;
+    map['total_results'] = _totalResults;
+    return map;
+  }
+
+}
+
+/// adult : false
+/// backdrop_path : "/k1KrbaCMACQiq7EA0Yhw3bdzMv7.jpg"
+/// genre_ids : [16,10751,10402,14,35]
+/// id : 901362
+/// original_language : "en"
+/// original_title : "Trolls Band Together"
+/// overview : "전 세계 모든 트롤을 열광케 했던 최고의 아이돌 그룹 ‘브로존’. 역대급 무대 실수와 형제 간의 불화로 결국 해체한 뒤, 모두에게 잊혀 간다. 그러던 어느 날, ‘브로존’의 황금막내 ‘브랜치’는 메인보컬 ‘플로이드’가 슈퍼스타 ‘벨벳’과 ‘비니어’에게 잡혀 재능을 빼앗기고 있다는 소식을 듣는다. 그를 구하기 위해서는 흩어져 있는 ‘브로존’을 재결합하고 완벽한 화음을 되찾아야 하는데… 12월, 가장 짜릿한 컴백 무대의 시작!"
+/// popularity : 968.582
+/// poster_path : "/7M2pc9OboapgtoBbkU49Aim7O5B.jpg"
+/// release_date : "2023-10-12"
+/// title : "트롤 밴드 투게더"
+/// video : false
+/// vote_average : 7.169
+/// vote_count : 355
+
+class Results {
+  Results({
+    bool? adult,
+    String? backdropPath,
+    List<num>? genreIds,
+    num? id,
+    String? originalLanguage,
+    String? originalTitle,
+    String? overview,
+    num? popularity,
+    String? posterPath,
+    String? releaseDate,
+    String? title,
+    bool? video,
+    num? voteAverage,
+    num? voteCount,}) {
+    _adult = adult;
+    _backdropPath = backdropPath;
+    _genreIds = genreIds;
+    _id = id;
+    _originalLanguage = originalLanguage;
+    _originalTitle = originalTitle;
+    _overview = overview;
+    _popularity = popularity;
+    _posterPath = posterPath;
+    _releaseDate = releaseDate;
+    _title = title;
+    _video = video;
+    _voteAverage = voteAverage;
+    _voteCount = voteCount;
+  }
+
+  Results.fromJson(dynamic json) {
+    _adult = json['adult'];
+    _backdropPath = json['backdrop_path'];
+    _genreIds = json['genre_ids'] != null ? json['genre_ids'].cast<num>() : [];
+    _id = json['id'];
+    _originalLanguage = json['original_language'];
+    _originalTitle = json['original_title'];
+    _overview = json['overview'];
+    _popularity = json['popularity'];
+    _posterPath = json['poster_path'];
+    _releaseDate = json['release_date'];
+    _title = json['title'];
+    _video = json['video'];
+    _voteAverage = json['vote_average'];
+    _voteCount = json['vote_count'];
+  }
+
+  bool? _adult;
+  String? _backdropPath;
+  List<num>? _genreIds;
+  num? _id;
+  String? _originalLanguage;
+  String? _originalTitle;
+  String? _overview;
+  num? _popularity;
+  String? _posterPath;
+  String? _releaseDate;
+  String? _title;
+  bool? _video;
+  num? _voteAverage;
+  num? _voteCount;
+
+  Results copyWith({ bool? adult,
+    String? backdropPath,
+    List<num>? genreIds,
+    num? id,
+    String? originalLanguage,
+    String? originalTitle,
+    String? overview,
+    num? popularity,
+    String? posterPath,
+    String? releaseDate,
+    String? title,
+    bool? video,
+    num? voteAverage,
+    num? voteCount,
+  }) =>
+      Results(adult: adult ?? _adult,
+        backdropPath: backdropPath ?? _backdropPath,
+        genreIds: genreIds ?? _genreIds,
+        id: id ?? _id,
+        originalLanguage: originalLanguage ?? _originalLanguage,
+        originalTitle: originalTitle ?? _originalTitle,
+        overview: overview ?? _overview,
+        popularity: popularity ?? _popularity,
+        posterPath: posterPath ?? _posterPath,
+        releaseDate: releaseDate ?? _releaseDate,
+        title: title ?? _title,
+        video: video ?? _video,
+        voteAverage: voteAverage ?? _voteAverage,
+        voteCount: voteCount ?? _voteCount,
+      );
+
+  bool? get adult => _adult;
+
+  String? get backdropPath => _backdropPath;
+
+  List<num>? get genreIds => _genreIds;
+
+  num? get id => _id;
+
+  String? get originalLanguage => _originalLanguage;
+
+  String? get originalTitle => _originalTitle;
+
+  String? get overview => _overview;
+
+  num? get popularity => _popularity;
+
+  String? get posterPath => _posterPath;
+
+  String? get releaseDate => _releaseDate;
+
+  String? get title => _title;
+
+  bool? get video => _video;
+
+  num? get voteAverage => _voteAverage;
+
+  num? get voteCount => _voteCount;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['adult'] = _adult;
+    map['backdrop_path'] = _backdropPath;
+    map['genre_ids'] = _genreIds;
+    map['id'] = _id;
+    map['original_language'] = _originalLanguage;
+    map['original_title'] = _originalTitle;
+    map['overview'] = _overview;
+    map['popularity'] = _popularity;
+    map['poster_path'] = _posterPath;
+    map['release_date'] = _releaseDate;
+    map['title'] = _title;
+    map['video'] = _video;
+    map['vote_average'] = _voteAverage;
+    map['vote_count'] = _voteCount;
+    return map;
+  }
+
+}
+
+/// maximum : "2024-01-10"
+/// minimum : "2023-12-20"
+
+class Dates {
+  Dates({
+    String? maximum,
+    String? minimum,}) {
+    _maximum = maximum;
+    _minimum = minimum;
+  }
+
+  Dates.fromJson(dynamic json) {
+    _maximum = json['maximum'];
+    _minimum = json['minimum'];
+  }
+
+  String? _maximum;
+  String? _minimum;
+
+  Dates copyWith({ String? maximum,
+    String? minimum,
+  }) =>
+      Dates(maximum: maximum ?? _maximum,
+        minimum: minimum ?? _minimum,
+      );
+
+  String? get maximum => _maximum;
+
+  String? get minimum => _minimum;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['maximum'] = _maximum;
+    map['minimum'] = _minimum;
+    return map;
+  }
+
+}
