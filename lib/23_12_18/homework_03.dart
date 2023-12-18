eae76a4eb588dd2bb6c5c9ccea9d7f44e710ab91
kimkangtae <tt997768@gmail.com>
2023-12-18 15:40:54 +0900

    연습문제 3 findmax 테스트 코드 작성

---
+++ lib/23_12_18/homework_03.dart
@@ -0,0 +1,6 @@
+// 연습문제 3
+// 입력으로 숫자로 이루어진 리스트를 받아들이는 함수 findMax를 작성하시오
+
+import 'package:collection/collection.dart';
+
+int findMax(List<int> numbers) => numbers.max;
---
+++ test/homework_03_test.dart
@@ -0,0 +1,9 @@
+import 'package:dart_exam/23_12_18/homework_03.dart';
+import 'package:test/test.dart';
+
+// 연습문제 1
+void main() {
+  test('findMax() test', () {
+    expect(findMax([2,5,1,9,3]), 9);
+  });
+}
