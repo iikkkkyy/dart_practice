0e2315148325c1f8972cc0dc1489c7d2d41f40a2
kimkangtae <tt997768@gmail.com>
2023-12-18 15:01:10 +0900

    연습문제 1 코드 수정 전 tojson 테스트 코드

--- test/homework_01_test.dart
+++ test/homework_01_test.dart
@@ -7,17 +7,17 @@ import 'package:test/test.dart';
 void main() {
   
   test('bank json test', () {
-    expect(Bank.fromJson(jsonDecode(json)).name, '홍길동');
-    expect(Bank.fromJson(jsonDecode(json)).address, '서울시 어쩌구 저쩌구');
-    expect(Bank.fromJson(jsonDecode(json)).phone, '010-1111-2222');
+    Bank a = Bank('홍길동', '서울시 어쩌구 저쩌구', '010-1111-2222');
+    // expect(Bank.fromJson(jsonDecode(json)).name, '홍길동');
+    // expect(Bank.fromJson(jsonDecode(json)).address, '서울시 어쩌구 저쩌구');
+    // expect(Bank.fromJson(jsonDecode(json)).phone, '010-1111-2222');
+    expect(a.toJson(), jsonDecode(json));
   });
 }
 
 
-String json = '''
-{
+String json = '''{
  "name": "홍길동",
  "address": "서울시 어쩌구 저쩌구",
  "phone": "010-1111-2222"
-}
-''';
+}''';
