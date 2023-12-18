ecf6923347146a89cfee81fe938dbf5dfcf1883b
kimkangtae <tt997768@gmail.com>
2023-12-18 20:49:48 +0900

    연습문제 5 post repository 작성

---
+++ lib/23_12_18/homework_05_post_repository.dart
@@ -0,0 +1,8 @@
+import 'homework_05_post.dart';
+
+abstract interface class PostRepository {
+  Future<List<Post>> getPosts();
+  Future<void> addPost(Post post);
+  Future<void> deletePost(Post post);
+  Future<void> updatePost(Post post);
+}
