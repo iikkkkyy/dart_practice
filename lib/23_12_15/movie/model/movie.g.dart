dfadce784b0fdf22b32c90a455f16bc410cb30fa
kimkangtae <tt997768@gmail.com>
2023-12-19 09:46:12 +0900

    테스트파일

---
+++ lib/23_12_15/movie/model/movie.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'movie.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Result _$ResultFromJson(Map<String, dynamic> json) => Result(
+      originalTitle: json['originalTitle'] as String,
+      releaseDate: json['releaseDate'] as String,
+      title: json['title'] as String,
+    );
+
+Map<String, dynamic> _$ResultToJson(Result instance) => <String, dynamic>{
+      'originalTitle': instance.originalTitle,
+      'releaseDate': instance.releaseDate,
+      'title': instance.title,
+    };
