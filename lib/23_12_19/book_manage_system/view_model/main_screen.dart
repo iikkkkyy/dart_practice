8a8656fe04fdc6b103489d57f3eb4364daaf9ab9
kimkangtae <tt997768@gmail.com>
2023-12-19 21:18:23 +0900

    mainscreen 도서관리 추가

--- lib/23_12_19/book_manage_system/view_model/main_screen.dart
+++ lib/23_12_19/book_manage_system/view_model/main_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'package:dart_exam/23_12_19/book_manage_system/model/library.dart';
 import 'package:dart_exam/23_12_19/book_manage_system/repository/library_repository.dart';
+import 'package:dart_exam/23_12_19/book_manage_system/view_model/book_manage_screen.dart';
 import 'package:dart_exam/23_12_19/book_manage_system/view_model/user_manage_screen.dart';
 
 class MainScreen{
@@ -16,6 +17,7 @@ class MainScreen{
         UserManagementViewModel(_libraryRepository).userManagement();
         break;
       case '2':
+        BookManagementViewModel(_libraryRepository).bookManagement();
         break;
       default:
         print('시스템 off');
